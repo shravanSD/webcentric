00ca9592b5a1cfee774ba2815edba975fdf76852
prdlynx <dhondeapranav@gmail.com>
2017-12-20 11:52:42 +0400

    Updated index.php with relevant contents, not final.

--- CarZone LTD/index.php
+++ CarZone LTD/index.php
@@ -35,72 +35,52 @@
                 <section>
                     <table id="cont" cellspacing="30px">
                         <tr>
-                            <td>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis elit eu eros vehicula, ut imperdiet nunc convallis. 
-                                Morbi feugiat scelerisque consequat. Praesent porta, lorem ac rhoncus tempus, magna sapien dignissim metus, vel vehicula lectus erat ut sem. 
-                                Cras massa diam, vehicula nec nibh id, ullamcorper cursus urna. Nullam eleifend nunc in posuere maximus. Proin faucibus pellentesque ultrices. 
-                                Donec rhoncus libero nec metus rutrum, pretium consectetur lorem finibus. Aliquam vehicula malesuada ligula vitae convallis. Fusce id egestas nisi. 
-                                Aliquam erat volutpat. Quisque ac sagittis nibh, et mollis metus. Nunc fermentum aliquet dui, id porta massa. Aliquam finibus magna vel enim dignissim facilisis. 
-                                Cras in tempus quam.
+                            <td id="desc">
+                                <h3>Brief history &amp Info </h3>
+                                <p>We opened our dors in the fall of 2007 and thanks to you, our dear customers, we've grown a lot!  Our repair shops boast the latest equipment in car repair as well as trained personnel to take care of your cars' needs and ensure you are completely satisfied with our services.</p>
                             </td>
-                            <td>
-                                Vivamus et magna sem. Praesent convallis, lectus id pharetra elementum, lorem urna pharetra turpis, eget auctor nulla ante quis purus. 
-                                Integer non massa nibh. Aenean eu nisl eu quam laoreet vehicula. Curabitur eu diam vel mi feugiat tincidunt. Vestibulum viverra, elit sed commodo convallis, 
-                                sem lectus rhoncus orci, in eleifend augue metus porttitor justo. Phasellus semper gravida pretium.
-                            </td>
-                        </tr>
+                        </tr>    
+
                         <tr>
-                            <td>
-                                Mauris euismod orci et metus vestibulum, sit amet placerat ante lobortis. Etiam eu convallis odio. Sed dapibus, nisl sed efficitur blandit, 
-                                massa metus egestas elit, eu luctus nibh lorem ac velit. Phasellus accumsan nisl sed metus dapibus tempor. Mauris rutrum mi ex, in pulvinar est faucibus maximus. 
-                                Donec in orci vel arcu scelerisque faucibus id vitae mauris. Phasellus mattis, lacus at tristique rhoncus, tellus lectus ullamcorper urna, a tempor mauris odio 
-                                eget orci. Donec vestibulum, est nec elementum hendrerit, sem quam pellentesque urna, sit amet sodales tellus nibh at nisl. Nulla facilisi. Pellentesque lacus 
-                                enim, condimentum id orci eget, bibendum vehicula orci. Aliquam fringilla tincidunt suscipit. Donec sed tortor feugiat, aliquet nisl quis, convallis ipsum. 
-                                Nullam quis hendrerit velit, sed facilisis mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis et sapien 
-                                placerat quam rutrum commodo.
-                            </td>
-                            <td>
-                                Suspendisse feugiat ante vel mauris convallis congue. Donec porttitor, dui et euismod semper, dui eros tristique magna, in faucibus tortor risus eget sapien. 
-                                Curabitur lacinia egestas magna a euismod. Duis id augue nec lorem viverra lacinia vel at ligula. Phasellus urna dui, placerat sit amet diam sit amet, maximus 
-                                aliquam justo. Donec eu consectetur lorem, non pulvinar erat. Mauris id cursus nulla. Aliquam erat volutpat. Morbi at ipsum commodo, consectetur turpis eget, 
-                                tempus magna. Vivamus non nisl neque. Praesent quis arcu et mi euismod commodo et a velit. Cras volutpat sapien a dignissim hendrerit. Donec condimentum risus 
-                                vitae varius dignissim. Nam luctus magna nec lorem tristique, in suscipit lacus laoreet. Nulla facilisi.
-                            </td>
-                        </tr>
+                            <td id="services">
+                                <h3>Services</h3><br>
+                                <img src="images/Services/services.jpg" alt="services.jpg">
+                            </td> 
+                        </tr> 
+
                         <tr>
-                            <td>
-                                Praesent laoreet diam imperdiet elit porttitor, id consectetur felis venenatis. Vivamus varius non sem eget congue. Nulla vitae dui at nibh porttitor semper 
-                                sit amet ut massa. Mauris quis ex id magna tempor bibendum eu in sem. Nunc quam metus, commodo sit amet urna ut, euismod finibus ante. Integer tempus ante vitae 
-                                enim venenatis auctor vitae nec eros. Morbi blandit imperdiet nibh, quis volutpat nibh dignissim vitae. Phasellus porta purus et imperdiet lobortis. Maecenas 
-                                laoreet, ligula sit amet congue ornare, mauris augue imperdiet nulla, sed pellentesque turpis ligula euismod justo. Curabitur maximus nunc quam, nec suscipit 
-                                dolor rutrum sit amet. Curabitur sodales risus sit amet massa tempor, in porta massa tincidunt. Morbi augue sem, ultricies nec libero eu, faucibus rhoncus metus.
+                            <td id="services">
+                                <h3>Why Choose Us?</h3><br>
+                                <p>Over the years, we've built up our reputation and trust with our loyal clientele. Today, we are happy to have 5000 of you among us. Moreover, last year's customer satisfaction survey has yielded a 99% positive response which we are definitely proud of. </p>
+
+                                <p>Did we mention? We understand that not having your car for even one day may cause inconveniences, which is why our mechanics operate efficiently to get your car back to you as fast as possible in the best conditions</p>
+                            </td> 
+                        </tr> 
+
+
+
+<!--                         <tr>
+                            <td id="review">
+                                <h3>Reviews</h3>
+                                <img src="images/Reviews/speech-left.png" alt="speech-left.jpg" width="500px" id="speech-l">
+                                <img src="images/Reviews/speech-right.png" alt="speech-right.jpg" width="700px" id="speech-r">
                             </td>
-                            <td>
-                                Vivamus auctor sed turpis vitae interdum. In congue consequat sagittis. Proin sed felis accumsan augue malesuada volutpat id eu lectus. Nulla vel quam ut 
-                                eros placerat posuere vel eu elit. Mauris eu dui eros. Integer iaculis ac libero non elementum. Donec consequat nisl vel orci pretium, in efficitur purus 
-                                finibus. Praesent ac tristique felis, sit amet aliquam nisi. Donec dignissim laoreet ligula, quis sagittis elit pulvinar eget. Ut pellentesque vulputate diam, 
-                                sed accumsan dui tristique a. In suscipit ex eros, vitae vehicula nulla tempus vel. Morbi vitae neque auctor, dapibus tortor in, dictum tellus. Etiam cursus ut 
-                                nunc et fermentum. Sed purus ante, dapibus vitae posuere sed, congue quis elit.
-                            </td>
-                        </tr>
+                        </tr>  -->
+
                         <tr>
-                            <td>
-                                Quisque vel rhoncus dolor. Curabitur neque odio, pellentesque nec nulla id, lobortis vestibulum quam. Vestibulum imperdiet nibh ac ante vehicula, in 
-                                maximus lacus commodo. Suspendisse accumsan elit vitae blandit bibendum. Integer imperdiet nibh ut nunc finibus accumsan. Aliquam rutrum luctus dolor 
-                                imperdiet posuere. Praesent euismod suscipit nisi faucibus fringilla. Nunc ex erat, laoreet aliquet tincidunt sit amet, convallis id dui. Morbi vel tristique 
-                                lacus. Donec porta felis ultrices sem dignissim, et maximus magna viverra. In hac habitasse platea dictumst. Morbi id pretium nibh, non ultricies ligula. 
-                                Phasellus dignissim lorem augue, et pretium arcu tincidunt sit amet.
-                            </td>
-                            <td>
-                                Donec dignissim dictum dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis 
-                                dis parturient montes, nascetur ridiculus mus. Aenean iaculis, nunc ut congue ornare, justo nisl sagittis eros, ut lobortis nisl tellus quis ante. Ut dapibus 
-                                bibendum augue vitae porttitor. Maecenas vitae luctus tortor. Cras quam ex, auctor eu iaculis at, cursus quis quam. Nulla tristique vestibulum lobortis. 
-                                Suspendisse eleifend, lorem sed pellentesque placerat, justo augue faucibus mi, sit amet imperdiet lectus orci sed tellus. Integer iaculis, lacus eu aliquam 
-                                luctus, erat dui imperdiet diam, eu pretium erat orci et massa. Integer laoreet varius leo ac elementum. Aenean et vehicula diam. Sed tristique lacus enim, 
-                                eu ullamcorper massa laoreet ac. Integer eu lacinia eros.
-                            </td>
-                        </tr>
-                       
+                            <td id="help">
+                                <h3>Need any help?</h3>
+                                    <p>Hey there! Are you having any issues with your car lately? Don't know who to ask for help? No worries! We've got you covered. Contact our experts <span><a href="contact.php">here</a></span>and we'll be happy to answer your questions <strong>FREE</strong> of charge OR <span><a href="bookingserv.php"></span>book an appointment should you require any of the services we offer.</a> </p>
+                                </td>
+                        </tr>   
+
+                        <tr>
+                            <td id="services">
+                                <h3>Satisfied with our services?</h3>
+                                <p>Leave us feedback <a href="customerfeedback.php">here</a>.</p>
+                            </td> 
+                        </tr> 
+
                     </table>
                 </section>
             </div>
